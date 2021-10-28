9946e36d0562bbf6429bee8130585af81db78eb3
Miguel Bayon <miguel.bayon@iessanandres.com>
2021-10-28 12:24:06 +0200

    Cambios en local

--- App.java
+++ App.java
@@ -1,9 +1,11 @@
 public class App {
   
   public static void main(String[] args) {
+    System.out.println("Miguel Bayon");
     System.out.println("Contenido 2a");
     System.out.println("Contenido 3");
     System.out.println("Contenido 2b");
+    System.out.println("28 octubre 2021");
   }
   
 }
